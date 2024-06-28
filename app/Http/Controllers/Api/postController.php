1429ded1743609540b93bb98ebe887f5a66ac61b
NAZ <112337066+Nas-Z@users.noreply.github.com>
2024-06-29 00:22:02 +0300

    Creating Posts table and Post model. Adding postController and index func to show all posts.

---
+++ app/Http/Controllers/Api/postController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Post; 
+
+class postController extends Controller {
+
+    public function index() {
+
+        return Post::all();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\signupController;
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\PostController;
 
 
 /*
@@ -21,7 +22,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 // register 
-Route::post('user/signup', [signupController::class, 'register']);
+Route::post('/user/signup', [signupController::class, 'register']);
 
-
+//login
 Route::post('/user/login', [LoginController::class, 'loginUser']);
+
+//index
+Route::get('/posts', [PostController::class, 'index']);
+
